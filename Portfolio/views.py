3ae510e29021aff914465b7a829cb1f5280a5041
Dillon1john <Dillon1john@gmail.com>
2025-04-10 22:40:24 -0400

    added template ListView for projects

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
+from .models import Project
 
 
 # Create your views here.
@@ -14,3 +15,18 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home'
         return context
+
+
+class ProjectListView(TemplateView):
+    template_name = 'Portfolio/allProjects.html'
+    model = Project
+    context_object_name = 'projects'
+    paginate_by = 6
+
+
+class ProjectDetailView(TemplateView):
+    template_name = 'Portfolio/projectDetail.html'
+    model = Project
+    context_object_name = 'project'
+
+
