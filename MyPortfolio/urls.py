39058d63f99c7e80f24266728e63b91cc9527a1c
Dillon1john <Dillon1john@gmail.com>
2025-04-10 22:39:08 -0400

    uncommented admin urls

--- MyPortfolio/urls.py
+++ MyPortfolio/urls.py
@@ -21,6 +21,6 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+       path('admin/', admin.site.urls),
     path('', include('Portfolio.urls')),
 ]
