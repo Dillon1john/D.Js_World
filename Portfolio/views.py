5aade2fd863bc8eeeccf260a121ba647d02389cc
Dillon1john <Dillon1john@gmail.com>
2025-05-22 23:33:01 -0400

    Added split and sort function to project detail class Added context details to add in technologies for details page

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
-from .models import Project
+from django.views.generic import TemplateView,  ListView, DetailView
+from .models import *
 
 
 # Create your views here.
@@ -17,16 +17,36 @@ class HomeView(TemplateView):
         return context
 
 
-class ProjectListView(TemplateView):
-    template_name = 'Portfolio/allProjects.html'
+class ProjectListView(ListView):
     model = Project
+    template_name = 'Portfolio/allProjects.html'
+    ordering = ['title']
     context_object_name = 'projects'
     paginate_by = 6
+    if model:
+        print('Model found')
 
 
-class ProjectDetailView(TemplateView):
+
+class ProjectDetailView(DetailView):
     template_name = 'Portfolio/projectDetail.html'
     model = Project
+    technologies = Project.technologies
     context_object_name = 'project'
+
+    def split_and_sort(self, input_string):
+        # Split the string by comma and strip whitespace
+        split_list = [item.strip() for item in input_string.split(',')]
+        # Return sorted list
+        return sorted(split_list)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project_id = self.kwargs['pk']
+        technologies = Project.objects.get(pk=project_id).technologies
+        context['technologies'] = self.split_and_sort(technologies)
+        return context
+
+
 
 
