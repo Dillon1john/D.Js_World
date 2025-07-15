ac591f3c51bb1fd0a393b82b62c6b779139fdd9e
Dillon1john <Dillon1john@gmail.com>
2025-07-15 17:56:57 -0400

    added url for contact view

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='start'),
     path('projects/', views.ProjectListView.as_view(), name='projects'),
-
+    path('contact/', views.ContactView.as_view(), name='contact'),
     path('projects/<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
 
 ]
